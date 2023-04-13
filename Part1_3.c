5a99d0a1f55f05d877a7e338cf91cfaf7df70d65
Alexas <konstantinosa57@gmail.com>
2023-04-14 00:08:16 +0300

    Functional value/cumulative search by date
    ChatGPTis god, better find a way to rewrite this so we dont get detected by AI detectors...

---
+++ Part1_3.c
@@ -0,0 +1,309 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// Total number of records in the dataset
+#define NUM_RECORDS 111438
+
+// Structure representing a record from the dataset
+typedef struct {
+    char direction[20];
+    int year;
+    time_t date;
+    char weekday[10];
+    char country[50];
+    char commodity[100];
+    char transport_mode[20];
+    char measure[20];
+    long long value;
+    long long cumulative;
+} Record;
+
+// Structure representing an indexed record, we'll use that later to sort the results
+typedef struct {
+    int index;
+    Record record;
+} IndexedRecord;
+
+// Global array holding the indexed records
+IndexedRecord records[NUM_RECORDS];
+
+// Function prototypes
+void read_csv(const char *filename);
+void sort_records_by_date();
+time_t date_to_unix(const char *date_str);
+void parse_date(const char *date_str, int *day, int *month, int *year);
+int date_comparator(const void *a, const void *b);
+int binary_search(time_t target_date, int *found_indexes);
+int interpolation_search(time_t target_date, int *found_indexes);
+int compare_original_indexes(const void *a, const void *b);
+
+int main() {
+    // Read records from the CSV file
+    read_csv("C:/Users/user/Desktop/PROJECT/Records.csv");
+
+    // Sort records by date
+    sort_records_by_date();
+
+    // Loop until the user decides to exit
+    while (1) {
+        // Ask the user for the target date
+        char date[11];
+        printf("Enter a date (ex: 01/01/2015): ");
+        scanf("%10s", date);
+        time_t target_date = date_to_unix(date);
+
+        // Ask the user for the search method
+        int choice;
+        printf("Choose a search method:\n");
+        printf("1. Binary Search\n");
+        printf("2. Interpolation Search\n");
+        printf("Enter your choice (1 or 2): ");
+        scanf("%d", &choice);
+
+        // Start the clock to measure search time
+        clock_t start = clock();
+
+        // Perform the search and store the found indexes
+        int found_indexes[NUM_RECORDS];
+        int num_found = 0;
+
+        if (choice == 1) {
+            num_found = binary_search(target_date, found_indexes);
+        } else if (choice == 2) {
+            num_found = interpolation_search(target_date, found_indexes);
+        } else {
+            printf("Invalid choice!\n");
+            return 1;
+        }
+
+        // Display the search results
+        if (num_found > 0) {
+            // Sort found_indexes by original index values
+            qsort(found_indexes, num_found, sizeof(int), compare_original_indexes);
+
+            // Print the results
+            printf("\nDate: %s\n", date);
+            printf("-------------------------------------------------\n");
+            printf("%-10s %-20s %-20s\n", "File Index", "Value", "Cumulative");
+            printf("-------------------------------------------------\n");
+            for (int i = 0; i < num_found; i++) {
+                int original_index = records[found_indexes[i]].index;
+                printf("%-10d %-20lld %-20lld\n", original_index + 2, records[found_indexes[i]].record.value, records[found_indexes[i]].record.cumulative);
+            }
+            printf("-------------------------------------------------\n");
+        } else {
+            printf("\nDate not found.\n");
+        }
+
+    // Stop the clock and calculate the search time
+    clock_t end = clock();
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Search time: %f seconds\n", cpu_time_used);
+
+    // Ask the user if they want to search for another date
+    char search_again;
+    printf("Would you like to search for another date? (y/n): ");
+    scanf(" %c", &search_again);
+    if (search_again == 'n' || search_again == 'N') {
+        break;
+    }
+    }
+
+return 0;
+
+}
+
+// Function to read the CSV file and store the records in the global array
+void read_csv(const char *filename) {
+FILE *file = fopen(filename, "r");
+if (file == NULL) {
+fprintf(stderr, "Could not open file: %s\n", filename);
+exit(EXIT_FAILURE);
+}
+char line[1024];
+fgets(line, sizeof(line), file); // Skip header
+
+for (int i = 0; i < NUM_RECORDS && fgets(line, sizeof(line), file); i++) {
+    records[i].index = i;
+    char date_str[11];
+    sscanf(line, "%[^,],%d,%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%lld,%lld",
+           records[i].record.direction,
+           &records[i].record.year,
+           date_str,
+           records[i].record.weekday,
+           records[i].record.country,
+           records[i].record.commodity,
+           records[i].record.transport_mode,
+           records[i].record.measure,
+           &records[i].record.value,
+           &records[i].record.cumulative);
+    records[i].record.date = date_to_unix(date_str);
+}
+
+fclose(file);
+}
+
+// Function to sort records by date
+void sort_records_by_date() {
+qsort(records, NUM_RECORDS, sizeof(IndexedRecord), date_comparator);
+}
+
+// Function to convert a date string (dd/mm/yyyy) to a UNIX timestamp
+time_t date_to_unix(const char *date_str) {
+struct tm tm;
+memset(&tm, 0, sizeof(struct tm));
+int day, month, year;
+parse_date(date_str, &day, &month, &year);
+tm.tm_mday = day;
+tm.tm_mon = month - 1;
+tm.tm_year = year - 1900;
+return mktime(&tm);
+}
+
+// Function to parse a date string (dd/mm/yyyy) and return day, month, and year as integers
+void parse_date(const char *date_str, int *day, int *month, int *year) {
+sscanf(date_str, "%d/%d/%d", day, month, year);
+}
+
+// Date comparator function for qsort
+int date_comparator(const void *a, const void *b) {
+const IndexedRecord *record_a = (const IndexedRecord *)a;
+const IndexedRecord *record_b = (const IndexedRecord *)b;
+if (record_a->record.date < record_b->record.date) {
+return -1;
+} else if (record_a->record.date > record_b->record.date) {
+return 1;
+} else {
+return 0;
+}
+}
+
+// Function to perform binary search for a target date
+int binary_search(time_t target_date, int *found_indexes) {
+int left = 0;
+int right = NUM_RECORDS - 1;
+int num_found = 0;
+
+while (left <= right) {
+    int mid = left + (right - left) / 2;
+
+    // Check if the target date is at the middle position
+    if (target_date == records[mid].record.date) {
+        found_indexes[num_found++] = mid;
+
+        // Check for other records with the same date in the left and right of the middle position
+        int temp = mid - 1;
+        while (temp >= 0 && records[temp].record.date == target_date) {
+            found_indexes[num_found++] = temp;
+            temp--;
+        }
+        temp = mid + 1;
+        while (temp < NUM_RECORDS && records[temp].record.date == target_date) {
+            found_indexes[num_found++] = temp;
+            temp++;
+        }
+        break;
+    } else if (target_date < records[mid].record.date) {
+        right = mid - 1;
+    } else {
+        left = mid + 1;
+    }
+}
+
+return num_found;
+
+}
+
+// Function to perform interpolation search for a target date
+int interpolation_search(time_t target_date, int *found_indexes) {
+int left = 0;
+int right = NUM_RECORDS - 1;
+int num_found = 0;
+
+// Continue searching while the target date is within the search range
+while (left <= right && target_date >= records[left].record.date && target_date <= records[right].record.date) {
+    if (left == right) {
+        if (records[left].record.date == target_date) {
+            found_indexes[num_found++] = left;
+        }
+        break;
+    }
+
+    // Calculate the estimated position of the target date
+    int pos = left + (((double)(right - left) / (records[right].record.date - records[left].record.date)) * (target_date - records[left].record.date));
+
+    // Check if the target date is at the estimated position
+    if (target_date == records[pos].record.date) {
+        found_indexes[num_found++] = pos;
+
+        // Check for other records with the same date in the left and right of the estimated position
+        int temp = pos - 1;
+        while (temp >= 0 && records[temp].record.date == target_date) {
+            found_indexes[num_found++] = temp;
+            temp--;
+        }
+        temp = pos + 1;
+        while (temp < NUM_RECORDS && records[temp].record.date == target_date) {
+            found_indexes[num_found++] = temp;
+            temp++;
+        }
+        break;
+    } else if (target_date < records[pos].record.date) {
+        right = pos - 1;
+    } else {
+        left = pos + 1;
+    }
+}
+
+return num_found;
+
+}
+
+// Function to compare original indexes of records
+int compare_original_indexes(const void *a, const void *b) {
+int index_a = *(const int *)a;
+int index_b = *(const int *)b;
+int original_index_a = records[index_a].index;
+int original_index_b = records[index_b].index;
+
+    if (original_index_a < original_index_b) {
+        return -1;
+    } else if (original_index_a > original_index_b) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
+
+/* Compare integers for sorting found_indexes array */
+int compare_int(const void *a, const void *b) {
+int int_a = *(const int *)a;
+int int_b = *(const int *)b;
+
+if (int_a < int_b) {
+    return -1;
+} else if (int_a > int_b) {
+    return 1;
+} else {
+    return 0;
+}
+
+}
+
+/* Compare IndexedRecord structs for sorting records array */
+int compare_indexed_records(const void *a, const void *b) {
+const IndexedRecord *record_a = (const IndexedRecord *)a;
+const IndexedRecord *record_b = (const IndexedRecord *)b;
+
+if (record_a->record.date < record_b->record.date) {
+    return -1;
+} else if (record_a->record.date > record_b->record.date) {
+    return 1;
+} else {
+    return record_a->index - record_b->index;
+}
+
+}
